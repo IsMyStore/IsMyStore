5f03af55fcd6fdfc7ee5d2c664c3b40cef9fdeb5
Gaëtan H <steellgold0@gmail.com>
2022-05-15 21:44:03 +0200

    Introduce PRICING, HOME

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController {
+
+	/**
+	 * @Route("/", name="home")
+	 */
+	public function index() : Response {
+		return $this->render('base/home.html.twig', [
+			"current_page" => "default"
+		]);
+	}
+
+	/**
+	 * @Route("/pricing", name="pricing")
+	 */
+	public function pricing() : Response {
+		return $this->render('base/pricing.html.twig', [
+			"current_page" => "pricing"
+		]);
+	}
+
+	/**
+	 * @Route("/contact", name="contact")
+	 */
+	public function contact() : Response {
+		return $this->render('base/contact.html.twig', [
+			"current_page" => "contact"
+		]);
+	}
+}
