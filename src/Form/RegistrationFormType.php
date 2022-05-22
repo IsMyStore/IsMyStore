2ae15e26aa6398cd8362acd10445f36164972a92
Gaëtan H <steellgold0@gmail.com>
2022-05-22 09:36:51 +0200

    Add IsMyStore/CheckIP package for default selected country

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use ismystore\checkip\CheckIP;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -16,7 +17,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 class RegistrationFormType extends AbstractType {
 
 	public function buildForm(FormBuilderInterface $builder, array $options): void {
-		dump($_SERVER['REMOTE_ADDR']);
+		$checkIP = new CheckIP($_SERVER['REMOTE_ADDR']);;
 		$builder
 			->add("firstName",TextType::class,[
 				'label' => false,
@@ -41,7 +42,7 @@ class RegistrationFormType extends AbstractType {
 				'attr' => [
 					'placeholder' => 'Country',
 				],
-				'data' => $this->ip_info($_SERVER['REMOTE_ADDR'],"Country Code"),
+				'data' => $checkIP->getCountryCode(),
 			])
 			->add('plainPassword', PasswordType::class, [
 				'label' => false,
@@ -64,70 +65,5 @@ class RegistrationFormType extends AbstractType {
 		$resolver->setDefaults([
 			'data_class' => User::class,
 		]);
-	}
-
-	function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE): array|string|null {
-		$output = NULL;
-		if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
-			$ip = $_SERVER["REMOTE_ADDR"];
-			if ($deep_detect) {
-				if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
-					$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-				if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
-					$ip = $_SERVER['HTTP_CLIENT_IP'];
-			}
-		}
-		$purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
-		$support    = array("country", "countrycode", "state", "region", "city", "location", "address");
-		$continents = array(
-			"AF" => "Africa",
-			"AN" => "Antarctica",
-			"AS" => "Asia",
-			"EU" => "Europe",
-			"OC" => "Australia (Oceania)",
-			"NA" => "North America",
-			"SA" => "South America"
-		);
-		if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
-			$ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
-			if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
-				switch ($purpose) {
-					case "location":
-						$output = array(
-							"city"           => @$ipdat->geoplugin_city,
-							"state"          => @$ipdat->geoplugin_regionName,
-							"country"        => @$ipdat->geoplugin_countryName,
-							"country_code"   => @$ipdat->geoplugin_countryCode,
-							"continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
-							"continent_code" => @$ipdat->geoplugin_continentCode
-						);
-						break;
-					case "address":
-						$address = array($ipdat->geoplugin_countryName);
-						if (@strlen($ipdat->geoplugin_regionName) >= 1)
-							$address[] = $ipdat->geoplugin_regionName;
-						if (@strlen($ipdat->geoplugin_city) >= 1)
-							$address[] = $ipdat->geoplugin_city;
-						$output = implode(", ", array_reverse($address));
-						break;
-					case "city":
-						$output = @$ipdat->geoplugin_city;
-						break;
-					case "state":
-						$output = @$ipdat->geoplugin_regionName;
-						break;
-					case "region":
-						$output = @$ipdat->geoplugin_regionName;
-						break;
-					case "country":
-						$output = @$ipdat->geoplugin_countryName;
-						break;
-					case "countrycode":
-						$output = @$ipdat->geoplugin_countryCode;
-						break;
-				}
-			}
-		}
-		return $output;
 	}
 }
